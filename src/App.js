d2e89b45780251ed7bf5716a88271eced67bfac9
surajverma2587 <surajverma2587@gmail.com>
2024-10-16 09:32:20 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export const App = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   const [data, setData] = useState();
+  const [username, setUsername] = useState("surajverma2587");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -12,11 +13,13 @@ export const App = () => {
 
       try {
         const response = await axios.get(
-          "https://api.github.com/users/surajverma2587"
+          `https://api.github.com/users/${username}`
         );
 
+        setIsError(false);
         setData(response.data);
       } catch (error) {
+        setData();
         setIsError(true);
       }
 
@@ -24,13 +27,36 @@ export const App = () => {
     };
 
     fetchData();
-  }, []);
+  }, [username]);
+
+  const handleClick = () => {
+    setUsername("zoelaventhol");
+  };
 
   return (
     <div>
       {isLoading && <div>Loading...</div>}
       {isError && <div>Oops something went wrong</div>}
-      {data && <div>Data received</div>}
+      {data && (
+        <div>
+          <button onClick={handleClick}>Zoe</button>
+          <div className="card" style={{ width: "18rem" }}>
+            <img
+              src={data.avatar_url}
+              className="card-img-top"
+              alt={data.name}
+            />
+            <div className="card-body">
+              <h5 className="card-title">{data.name}</h5>
+              <h6 className="card-text">{data.location}</h6>
+              <p className="card-text">{data.bio}</p>
+              <a href={data.html_url} className="btn btn-primary">
+                GitHub
+              </a>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
